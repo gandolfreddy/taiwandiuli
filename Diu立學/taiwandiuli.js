b0fa38319a70bad60d8bf79e99a07cdfcabbfe34
Gandolfreddy <freddy.wei0810@gmail.com>
2022-02-25 22:34:10 +0800

    update content.
    as title.

--- Diu立學/taiwandiuli.js
+++ Diu立學/taiwandiuli.js
@@ -8,6 +8,8 @@ function showResult() {
     if (date) {
         if (date === "1992-12-16") {
             resultText = "妳是個，びじん";
+        } else if (date === "1996-10-21") { 
+            resultText = "Diu 立學研究家の誕生日";
         } else {
             resultText = "乎資です";
         }
